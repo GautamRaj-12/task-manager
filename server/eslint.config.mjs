65939037a26c290951970387d1c9f7e9295acefd
Gautam Raj <gautamraj996@gmail.com>
2024-07-29 22:25:48 +0530

    Add eslint-config-prettier

--- server/eslint.config.mjs
+++ server/eslint.config.mjs
@@ -1,13 +1,15 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import esLintConfigPrettier from 'eslint-config-prettier'
 
 export default [
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
+  esLintConfigPrettier,
   {
     rules: {
-      "no-unused-vars": "warn",
-      "no-undef": "warn",
+      'no-unused-vars': 'warn',
+      'no-undef': 'warn',
     },
   },
-];
+]

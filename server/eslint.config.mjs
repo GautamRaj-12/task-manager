f2d3981ee046e51d854ea7cb32202f299d68b6ad
Gautam Raj <gautamraj996@gmail.com>
2024-07-29 22:22:43 +0530

    Revert "Add eslint-config-prettier"
    This reverts commit 322c91003ddc7ba7afdefe2fe80001d709088b5e.

--- server/eslint.config.mjs
+++ server/eslint.config.mjs
@@ -1,15 +1,13 @@
-import globals from 'globals'
-import pluginJs from '@eslint/js'
-import eslintConfigPrettier from 'eslint-config-prettier'
+import globals from "globals";
+import pluginJs from "@eslint/js";
 
 export default [
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
-  eslintConfigPrettier,
   {
     rules: {
-      'no-unused-vars': 'warn',
-      'no-undef': 'warn',
+      "no-unused-vars": "warn",
+      "no-undef": "warn",
     },
   },
-]
+];

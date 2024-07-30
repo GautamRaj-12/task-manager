3815da26beaec02771a85f0872a9fed693a2424d
Gautam Raj <gautamraj996@gmail.com>
2024-07-30 22:39:56 +0530

    Add task controller logic for adding, fetching and deleting

--- server/src/controllers/task.controller.js
+++ server/src/controllers/task.controller.js
@@ -4,17 +4,36 @@ import { ApiResponse } from '../utils/apiResponse.js'
 import { Task } from '../models/task.model.js'
 
 const addTask = asyncHandler(async (req, res) => {
-  return res.status(200).json(new ApiResponse(200, {}, 'Add Task Route'))
+  const { title, description, status, priority, deadline, createdBy } = req.body
+  if (!title) throw new ApiError(400, 'Title is Required')
+  if (!status) throw new ApiError(400, 'Status is Required')
+
+  const createdTask = await Task.create({
+    title,
+    description,
+    status,
+    priority,
+    deadline,
+    createdBy,
+  })
+  return res
+    .status(200)
+    .json(new ApiResponse(200, { createdTask }, 'Add Task Route'))
 })
 
 const fetchSingleTask = asyncHandler(async (req, res) => {
+  const taskId = req.params.id
+  const fetchedPost = await Task.findById(taskId)
   return res
     .status(200)
-    .json(new ApiResponse(200, {}, 'Fetch Single Task Route'))
+    .json(new ApiResponse(200, { fetchedPost }, 'Task fetched successfully'))
 })
 
 const fetchAllTasks = asyncHandler(async (req, res) => {
-  return res.status(200).json(new ApiResponse(200, {}, 'Fetch All Tasks Route'))
+  const allTasks = await Task.find()
+  return res
+    .status(200)
+    .json(new ApiResponse(200, { allTasks }, 'All tasks fetched successfully'))
 })
 
 const updateTask = asyncHandler(async (req, res) => {
@@ -22,10 +41,13 @@ const updateTask = asyncHandler(async (req, res) => {
 })
 
 const deleteSingleTask = asyncHandler(async (req, res) => {
+  const taskId = req.params.id
+  const deletedTask = await Task.deleteOne({ _id: taskId })
   return res
     .status(200)
-    .json(new ApiResponse(200, {}, 'Delete Single Task Route'))
+    .json(new ApiResponse(200, { deletedTask }, 'Task Deleted'))
 })
+
 const deleteAllTasks = asyncHandler(async (req, res) => {
   return res
     .status(200)
